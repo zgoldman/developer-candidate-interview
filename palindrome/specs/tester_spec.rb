ec7b01d6bc9dc4535e36478a6941db61faa9c498
Jason Rogers <jacaetevha@gmail.com>
2016-03-31 17:46:22 -0400

    add stubs for palindrome and the web-app directory

---
+++ palindrome/specs/tester_spec.rb
@@ -0,0 +1,51 @@
+require 'rspec'
+require_relative '../tester'
+
+RSpec.describe Tester do
+  [Tester::T1, Tester::T2].each do |clz|
+    context 'words' do
+      it 'should be a palindrome' do
+        expect(clz.new.palindrome?('abcdefedcba')).to be_true
+      end
+
+      it 'should not be a palindrome' do
+        expect(clz.new.palindrome?('hbcdefedcbg')).to be_false
+      end
+    end
+
+    context 'phrases' do
+      it 'should be a palindrome' do
+        expect(clz.new.palindrome?('A man, a plan, a canal, Panama!!')).to be_true
+      end
+
+      it 'should not be a palindrome' do
+        expect(clz.new.palindrome?(%q{
+          So sorry Mrs. Fluberdeedupe -- my dog ate my homework. If I had but known
+          that you were serious about collecting it today, I wouldn't have covered
+          it in au jus and put it in his bowl. Next time, I'll just assume that you
+          mean for us to actually do the homework and turn it in. Is there anything
+          I can do to make it up to you, or to improve my grade?
+
+          ...
+
+          Call my parents? No, no... I don't think that's necessary. They're already
+          mad at me for not making Fido eat some brocolli with it!
+        })).to be_false
+      end
+    end
+
+    context 'edge cases' do
+      it 'should handle nil strings' do
+        expect(clz.new.palindrome?(nil)).to be_false
+      end
+
+      it 'should handle empty strings' do
+        expect(clz.new.palindrome?('')).to be_false
+      end
+
+      it 'should handle non-strings' do
+        expect(clz.new.palindrome?(100)).to be_false
+      end
+    end
+  end
+end
---
+++ palindrome/tester.rb
@@ -0,0 +1,13 @@
+class Tester
+  class T1
+    def palindrome?(string)
+      # first implementation
+    end
+  end
+
+  class T2
+    def palindrome?(string)
+      # second implementation
+    end
+  end
+end
